85c7077d78a914dfe9e4aa66066f85ea8efc2f11
mete <148999436+metetolga@users.noreply.github.com>
2024-12-03 21:53:33 +0300

    readme not finalized

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
 # Basic-spell-checker
  
+This repository contains minimum edit distance algorithm to find spell-checked versions of words.
+
+`utils.py` contains all necessary functions and besides you only need numpy. 
+
+Let's start with the functions of `utils.py`:
+
+## Functions
+
+### process_data
+
+Processing the given file and returns all words that lowered. Finding words are provided by `\w+` regular expression.
+This matches words with lowercase, uppercase, digit and underscore characters.
+
+### get_counter 
+
+Gives counter dictionary of all word. 
+
+### get_probs 
+
+A probability of word is equal to how many times that word appeared divided by sum of total words.
+
+### edit_one_letter
+
+This function gives you to one edit version of every word.
+That can be happend with 4 interaction: Deletion, switching, replacing, insertion.
+
+1) split
+
+splitting word through every character gives us left and right split. For example:
+
+**word**: play
+
+**splits**: [('', 'play'), ('p', 'lay'), ('pl', 'ay'), ('pla', 'y'), ('play', '')]
+
+2) delete
+
+Deletion happens by removing every first element from the right split.
+
+**deletes**: ['lay', 'pay', 'ply', 'pla']
+
+3) switch
+
+switching happens by we swap first and second character of right split .
+
+**switches**: ['lpay', 'paly', 'plya']
+
+4) replaces
+
+
+**replaces**: ['aan', 'ban', 'caa', 'cab', 'cac', 'cad', 'cae', 'caf', 'cag', 'cah', 'cai', 'caj', 'cak', 'cal', 'cam', 'cao', 'cap', 'caq', 'car', 'cas', 'cat', 'cau', 'cav', 'caw', 'cax', 'cay', 'caz', 'cbn', 'ccn', 'cdn', 'cen', 'cfn', 'cgn', 'chn', 'cin', 'cjn', 'ckn', 'cln', 'cmn', 'cnn', 'con', 'cpn', 'cqn', 'crn', 'csn', 'ctn', 'cun', 'cvn', 'cwn', 'cxn', 'cyn', 'czn', 'dan', 'ean', 'fan', 'gan', 'han', 'ian', 'jan', 'kan', 'lan', 'man', 'nan', 'oan', 'pan', 'qan', 'ran', 'san', 'tan', 'uan', 'van', 'wan', 'xan', 'yan', 'zan'] 
+
+
---
+++ utils.py
@@ -0,0 +1,93 @@
+import re
+import numpy as np
+from collections import Counter 
+
+
+def process_data(file_name):
+    with open(file_name) as f:
+        file_name_data = f.read()
+    file_name_data=file_name_data.lower()
+    words = re.findall('\w+',file_name_data)
+    return words
+
+
+def get_counter(words):
+    return Counter(words)
+
+
+def get_probs(counter):
+    probs = {}
+    total = sum(counter.values())
+    for word in counter.keys():
+        probs[word] = counter[word] / total
+    return probs
+
+
+def edit_one_letter(word, verbose=False, allow_switches=False):
+    letters = 'abcdefghijklmnopqrstuvwxyz'
+
+    splits = [(word[:i], word[i:]) for i in range(len(word)+1)]
+
+    deletes = [L+R[1:] for L, R in splits if R]
+
+    switches = [L + R[1] + R[0] + R[2:] for L,R in splits if len(R) >= 2]      
+
+    replaces = []
+    for L, R in splits:
+        if not R:
+            continue
+        for char in letters:
+            ending = R[1:] if len(R) > 1 else ''
+            replaces.append(L + char + ending)
+
+    inserts = [ L + l + R for L,R in splits for l in letters]
+
+    if verbose: 
+        print(f"Input word {word}, \nsplit list = {splits}")
+        print(f"Delete:\n {deletes}")
+        print(f"Switches:\n {switches}")
+        print(f"Replaces:\n {replaces}")
+        print(f"Inserts:\n {inserts}")
+    res = set()
+    res.update(deletes)
+    res.update(inserts)
+    res.update(replaces)
+    if allow_switches:
+        res.update(switches)
+    return res 
+
+
+def edit_two_letters(word, allow_switches = False):
+    edit_two_set = set()
+    edit_one = edit_one_letter(word,allow_switches=allow_switches)
+    for w in edit_one:
+        if w:
+            edit_two = edit_one_letter(w,allow_switches=allow_switches)
+            edit_two_set.update(edit_two)
+
+    return edit_two_set
+
+
+def min_edit_distance(source, target, ins_cost = 1, del_cost = 1, rep_cost = 2):
+    m = len(source)
+    n = len(target)
+
+    D = np.zeros((m+1, n+1), dtype=int) 
+    
+    for row in range(1,m+1): 
+        D[row,0] = D[row-1,0] + del_cost
+
+    for col in range(1,n+1): 
+        D[0,col] = D[0,col-1] + ins_cost
+        
+    for row in range(1,m+1): 
+        for col in range(1,n+1):
+            r_cost = rep_cost
+            
+            if source[row-1] == target[col-1]:
+                r_cost = 0
+                
+            D[row,col] = min([D[row-1,col]+del_cost, D[row,col-1]+ins_cost, D[row-1,col-1]+r_cost])
+          
+    med = D[m,n]
+    return D, med
